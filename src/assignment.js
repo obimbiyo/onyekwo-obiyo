0ec010ef047e5449d8bba76af32b0a62d73180a9
obimbiyo <108768947+obimbiyo@users.noreply.github.com>
2022-07-15 19:41:28 +0000

    check challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -40,7 +40,7 @@ function countEvenNumbers(arrayOfNumbers) {
   let count = 0
   for(let i = 0; i < arrayOfNumbers.length;i++)
     {
-      if (arrayOfNumbers[i]% 2 ==0) {
+      if (arrayOfNumbers[i]% 2 ===0) {
         count++;
       }
     }
